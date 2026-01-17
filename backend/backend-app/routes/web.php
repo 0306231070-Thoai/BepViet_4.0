5d9af2f0f1dc2f994db55bc70b8e3150b7140174
0306231070-Thoai <0306231070@caothang.edu.vn>
2026-01-17 19:44:51 +0700

    tao api cho trang chu

--- backend/backend-app/routes/web.php
+++ backend/backend-app/routes/web.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('layouts.app');
-// });
 Route::get('/', function () {
-    return view('components.home.index'); // Trỏ tới file resources/views/home/index.blade.php
-})->name('home');
+    return view('layouts.app');
+});
