29f675d1ab8237e710bcef9c4c4d3d4e01fe7230
a <hello>
2026-01-22 21:27:16 +0700

    Admin PostController

--- backend/backend-app/app/Http/Controllers/Admin/AdminPostController.php
+++ backend/backend-app/app/Http/Controllers/Admin/AdminPostController.php
@@ -8,23 +8,20 @@ use App\Models\Blog;
 
 class AdminPostController extends Controller
 {
+    public function show($id)
+    {
+        $post = Blog::with([
+                'user:id,username',
+            ])
+            ->findOrFail($id);
+
+        return response()->json([
+            'success' => true,
+            'data' => $post
+        ], 200);
+    }
     public function index()
     {
         return Blog::with('user')->paginate(10);
-    }
-
-    public function approve($id)
-    {
-        $post = Blog::findOrFail($id);
-        $post->status = 'published';
-        $post->save();
-
-        return response()->json(['message'=>'Post approved']);
-    }
-
-    public function destroy($id)
-    {
-        Blog::destroy($id);
-        return response()->json(['message'=>'Post deleted']);
     }
 }
